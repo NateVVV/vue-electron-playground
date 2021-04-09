51b0b69b5d7da4bbeadbb7f8cb60a684dedd65c6
Nathanael Wenzel <nathanael.wenzel97@gmail.com>
2021-04-09 23:06:35 +0200

    show soritng component in Home.vue instead of HelloWorld

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,16 @@
 <template>
     <div class="home">
-        <HelloWorld msg="Welcome to Your Vue.js App" />
+        <Sorting></Sorting>
     </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Sorting from '@/components/Sorting.vue'
 
 export default {
     name: 'Home',
     components: {
-        HelloWorld
+        Sorting,
     }
 }
 </script>
