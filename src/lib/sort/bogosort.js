3bd78acf390763ba0a0dc1c06468f4a669e4ffc6
Nathanael Wenzel <nathanael.wenzel97@gmail.com>
2021-04-18 00:09:06 +0200

    add documentation

--- src/lib/sort/bogosort.js
+++ src/lib/sort/bogosort.js
@@ -1,5 +1,11 @@
 import { swap } from "@/lib/array.js";
 
+/**
+ * Sort an array in place
+ * @param {Array.<number>} elements The array which gets sorted
+ * @param {Object} swapDelay The delay for a swap operation
+ * @param {number} swapDelay.wait The delay for a swap operation
+ */
 async function bogosort(elements, swapDelay = { wait: 1 }) {
     while (!isSorted(elements)) {
         let index_a = Math.floor(Math.random() * elements.length);
@@ -8,6 +14,11 @@ async function bogosort(elements, swapDelay = { wait: 1 }) {
     }
 }
 
+/**
+ * Check if an array is sorted
+ * @param {Array.<number>} elements The array which is evaluated
+ * @returns {boolean} Inidicator if array is sorted or not
+ */
 function isSorted(elements) {
     for (let i = 0; i < elements.length - 1; i++) {
         if (elements[i] > elements[i + 1]) return false;
