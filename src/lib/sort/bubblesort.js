e600965bb13c7631a0a3bd5f5902adf55b91d5b3
Nathanael Wenzel <nathanael.wenzel97@gmail.com>
2021-04-18 00:14:49 +0200

    add documentation

--- src/lib/sort/bubblesort.js
+++ src/lib/sort/bubblesort.js
@@ -1,5 +1,11 @@
 import { swap } from "@/lib/array.js";
 
+/**
+ * Sort an array in place
+ * @param {Array.<number>} array The array which is sorted
+ * @param {Object} swapDelay The delay for a swap operation
+ * @param {number} swapDelay.wait The delay for a swap operation
+ */
 async function bubblesort(array, swapDelay = { wait: 1 }) {
     for (let n = array.length; n > 1; n--) {
         for (let i = 0; i < n - 1; i++) {
@@ -11,6 +17,14 @@ async function bubblesort(array, swapDelay = { wait: 1 }) {
     }
 }
 
+/**
+ * Sort an array in place.
+ * This can reduce some operation compared to normal bubblesort, when the array is sorted earlier
+ * than when iterated over each value
+ * @param {Array.<number>} array The array which is sorted
+ * @param {Object} swapDelay The delay for a swap operation
+ * @param {number} swapDelay.wait The delay for a swap operation
+ */
 async function improvedBubblesort(array, swapDelay = { wait: 1 }) {
     let swapped;
     let n = array.length;
