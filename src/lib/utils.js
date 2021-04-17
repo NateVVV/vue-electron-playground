182691a13f35159f1f4974dca19d27f39a4eb084
Nathanael Wenzel <nathanael.wenzel97@gmail.com>
2021-04-18 00:08:58 +0200

    add documentation

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,3 +1,7 @@
+/**
+ * Sleep for a specified time
+ * @param {number} ms The duration, how long to sleep [ms]
+ */
 const sleep = async (ms) =>
     await new Promise((resolve) => setTimeout(resolve, ms));
 
