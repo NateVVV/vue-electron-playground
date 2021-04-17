2ce6fe8545a9d84322fcc7ead84b9a1cef540b0a
Nathanael Wenzel <nathanael.wenzel97@gmail.com>
2021-04-17 13:27:47 +0200

    better structure cocktailsort

--- src/lib/sort/cocktailsort.js
+++ src/lib/sort/cocktailsort.js
@@ -5,24 +5,34 @@ async function cocktailsort(elements, swapDelay = { wait: 1 }) {
     let start = 0;
     let end = elements.length - 1;
     while (swapped) {
-        swapped = false;
-        for (let i = start; i < end; i++) {
-            if (elements[i] > elements[i + 1]) {
-                await swap(elements, i, i + 1, swapDelay);
-                swapped = true;
-            }
-        }
+        swapped = await bubblesortUpwards(elements, start, end, swapDelay);
         if (!swapped) break;
-        swapped = false;
         end--;
-        for (let i = end; i > start; i--) {
-            if (elements[i - 1] > elements[i]) {
-                await swap(elements, i, i - 1, swapDelay);
-                swapped = true;
-            }
-        }
+        swapped = await bubblesortDownwards(elements, end, start, swapDelay);
         start++;
     }
+}
+
+async function bubblesortUpwards(elements, from, to, swapDelay) {
+    let swapped = false;
+    for (let i = from; i < to; i++) {
+        if (elements[i] > elements[i + 1]) {
+            await swap(elements, i, i + 1, swapDelay);
+            swapped = true;
+        }
+    }
+    return swapped;
+}
+
+async function bubblesortDownwards(elements, from, to, swapDelay) {
+    let swapped = false;
+    for (let i = from; i > to; i--) {
+        if (elements[i - 1] > elements[i]) {
+            await swap(elements, i, i - 1, swapDelay);
+            swapped = true;
+        }
+    }
+    return swapped;
 }
 
 export { cocktailsort };
