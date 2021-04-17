d0120cf31ba90e8cb96a1391633baac59060539d
Nathanael Wenzel <nathanael.wenzel97@gmail.com>
2021-04-18 00:17:21 +0200

    adding documentation

--- src/lib/sort/shellsort.js
+++ src/lib/sort/shellsort.js
@@ -1,5 +1,11 @@
 import { swap } from "@/lib/array.js";
 
+/**
+ * Sort an array in place
+ * @param {Array.<number>} array The array which is sorted
+ * @param {Object} swapDelay The delay for a swap operation
+ * @param {number} swapDelay.wait The delay for a swap operation
+ */
 async function shellsort(array, swapDelay = { wait: 1 }) {
     const length = array.length;
     for (let gap = Math.floor(length / 2); gap > 0; gap = Math.floor(gap / 2)) {
