9d12630cd719b7dcae465a703fb547c0605053bb
Nathanael Wenzel <nathanael.wenzel97@gmail.com>
2021-04-18 11:07:38 +0200

    rework docu for array.js

--- src/lib/array.js
+++ src/lib/array.js
@@ -1,6 +1,11 @@
 import { sleep } from "@/lib/utils.js";
 
 /**
+ * @typedef {Object} Delay
+ * @property {number} wait The time to wait [ms]
+ */
+
+/**
  * Creates an array with random values. The maximum values is equal the size of the array
  * @param {number} size The size of the array
  * @returns the generated array
@@ -32,7 +37,7 @@ function filledArray(size) {
  * @param {Array.<number>} array The array in which the values get swapped
  * @param {number} first index of the first value
  * @param {number} second index of the second value
- * @param {number} wait time how long to wait after the values were swapped
+ * @param {Delay} delay time how long to wait after the values were swapped
  */
 async function swap(array, first, second, { wait = 1 } = {}) {
     const temp = array[first];
@@ -46,8 +51,7 @@ async function swap(array, first, second, { wait = 1 } = {}) {
  * @param {Array.<number>} array The array in which some valeus are shifted right
  * @param {number} from the index where to start with the shifting
  * @param {number} to the index where to end with the shifting
- * @param {Object} swapDelay The delay for the shift operation
- * @param {number} swapDelay.wait The delay for the shift operation [ms] 
+ * @param {Delay} swapDelay The delay for the shift operation
  */
 async function shiftRight(array, from, to, { wait = 1 } = {}) {
     const temp = array[to];
@@ -63,8 +67,7 @@ async function shiftRight(array, from, to, { wait = 1 } = {}) {
  * @param {Array.<number>} array The array in which some values are shifted to the left
  * @param {number} from The index where to start with the shifting
  * @param {number} to The index where to end with the shifting
- * @param {Object} swapDelay The delay for the shift operation
- * @param {number} swapDelay.wait The delay for the shift operation
+ * @param {Delay} swapDelay The delay for the shift operation
  */
 async function shiftLeft(array, from, to, { wait = 1 } = {}) {
     const temp = array[from];
