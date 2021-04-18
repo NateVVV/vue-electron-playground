199ac2ad40bed9dfc99562e193fe84d8340ae7d1
Nathanael Wenzel <nathanael.wenzel97@gmail.com>
2021-04-18 10:58:02 +0200

    adding documentation

--- src/lib/sort/quicksort.js
+++ src/lib/sort/quicksort.js
@@ -1,10 +1,23 @@
 import { swap } from "@/lib/array.js";
 
+/**
+ * Sort an array in place
+ * @param {Array.<number>} array The array which gets sorted
+ * @param {Object} swapDelay The delay for a swap operation
+ * @param {number} swapDelay.wait The delay for a swap operation [ms]
+ */
 async function quicksort(array, swapDelay = { wait: 1 }) {
     const length = array.length;
     await quicksortRecursive(array, 0, length - 1, swapDelay);
 }
 
+/**
+ * Sort an array in the range [left,right]
+ * @param {Array.<number>} elements The array which gets sorted
+ * @param {number} left left boundary of the range
+ * @param {number} right right boundary of the range
+ * @param {Object} swapDelay  The delay for a swap operation
+ */
 async function quicksortRecursive(elements, left, right, swapDelay) {
     // End of recursion reached?
     if (left >= right) return;
@@ -14,6 +27,14 @@ async function quicksortRecursive(elements, left, right, swapDelay) {
     await quicksortRecursive(elements, pivotPos + 1, right, swapDelay);
 }
 
+/**
+ * Rearange the given range [left,right] of an array into a lower part and a higher part
+ * @param {number[]} elements The array
+ * @param {number} left left boundary of the range
+ * @param {number} right right boundary of teh range
+ * @param {Object} swapDelay The delay for a swap operation
+ * @returns {number} The index of the array which divides the given range in a lower and a higher part
+ */
 async function divide(elements, left, right, swapDelay) {
     const pivot = elements[right];
     let i = left;
