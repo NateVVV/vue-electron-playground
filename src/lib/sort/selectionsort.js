aef55644a36e181775f058d36b6be74741ebc5b5
Nathanael Wenzel <nathanael.wenzel97@gmail.com>
2021-04-18 10:37:25 +0200

    adding documentation

--- src/lib/sort/selectionsort.js
+++ src/lib/sort/selectionsort.js
@@ -1,5 +1,17 @@
 import { swap, shiftRight } from "@/lib/array.js";
 
+/**
+ * @typedef {Object} ArrayElement
+ * @property {number} value The value of the element
+ * @property {number} index The index of the element in the corresponding array
+ */
+
+/**
+ * Sort an array in place using swap operation
+ * @param {Array.<number>} elements The array which gets sorted
+ * @param {Object} swapDelay The delay for a swap operation
+ * @param {number} swapDelay.wait The delay for a swap operation [ms]
+ */
 async function selectionsort(elements, swapDelay = { wait: 1 }) {
     let length = elements.length;
     for (let i = 0; i < length - 1; i++) {
@@ -9,6 +21,12 @@ async function selectionsort(elements, swapDelay = { wait: 1 }) {
     }
 }
 
+/**
+ * Sort an array in place using shift operation
+ * @param {Array.<number>} elements The array which gets sorted
+ * @param {Object} swapDelay The delay for a swap operation
+ * @param {number} swapDelay.wait The delay for a swap operation [ms]
+ */
 async function fastSelectionsort(elements, swapDelay = { wait: 1 }) {
     let length = elements.length;
     for (let i = 0; i < length - 1; i++) {
@@ -18,6 +36,13 @@ async function fastSelectionsort(elements, swapDelay = { wait: 1 }) {
     }
 }
 
+/**
+ * 
+ * @param {Array.<number>} elements The array which is investigated
+ * @param {number} from index where to start the search
+ * @param {number} to index where to end the search
+ * @returns {ArrayElement} The smallest element
+ */
 function findSmallestElementInRange(elements, from, to) {
     let smallesElement = { value: elements[from], index: from };
     for (let i = from + 1; i < to; i++) {
